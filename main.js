93e1b510db58ac3d70d62fd89899c9de7becd9e5
crazypeace <txican@gmail.com>
2023-06-18 00:30:20 +0800

    1

---
+++ main.js
@@ -0,0 +1,27 @@
+function convert() {
+  inputStr = document.querySelector("#githubScript").value;
+  if (inputStr == "") {
+    return;
+  }
+
+  ghproxy=document.querySelector("#ghproxy").value;
+  perlcmdbegin=' | ' + document.querySelector("#perlcmd").value + ' "$(curl -L ';
+  perlcmdend=')"';
+  perlrule='https://github.com/crazypeace/gh-proxy/raw/master/perl-pe-para';
+
+  // 先给裸的git类链接前面加上 https://
+  inputStr = inputStr.replace(/ git/g, ' https://git');
+  
+  // 再进行加github proxy的转换
+  regex = /(bash.*?curl.*?)(https?:\/\/.*?)(\).*)/s;
+  replacement = '$1' + ghproxy + '$2' + perlcmdbegin + ghproxy + perlrule + perlcmdend + '$3';
+
+  resultStr = inputStr.replace(regex, replacement);
+
+  document.querySelector("#resultWithGhproxy").value = resultStr;
+}
+
+function copyResult() {
+  resultStr = document.querySelector("#resultWithGhproxy").value;
+  navigator.clipboard.writeText(resultStr);
+}
